ad22b80ba3cf05e5aa942c453fb038a65ac4a974
Rajeshwari <rajrudra.raj@gmail.com>
2020-03-06 14:59:32 -0600

    add

--- scriptsrajeshwari.js
+++ scriptsrajeshwari.js
@@ -1,25 +1,27 @@
-/*console.log('INFO: Reading interaction.js');
+console.log('INFO: Reading values in function');
 
-console.log('INFO: Getting access to elements');
-
-const inputElementRadius = document.getElementById("radius")
-const inputElementResult = document.getElementById("result")
-
-const calcVolume = () => { 
-  
-  const textRad = inputElementRadius.value;
-  const Radius = parseFloat(textRad);
-
-  const Result = 2 * Math.PI * (Radius * Radius);
-  inputElementResult.innerHTML = "Result: " + Result;
-  return Radius;
-}
-
-const calcAreaTest = (radius) => {
-
-  return 2 * Math.PI * (radius * radius);
-}
-*/
+        
+            function euroConverter(){
+            document.converter.dollar.value = document.converter.euro.value * 1.470
+            document.converter.pound.value = document.converter.euro.value * 0.717
+            document.converter.yen.value = document.converter.euro.value * 165.192
+            }
+            function dollarConverter(){
+            document.converter.euro.value = document.converter.dollar.value * 0.680
+            document.converter.pound.value = document.converter.dollar.value * 0.488
+            document.converter.yen.value = document.converter.dollar.value * 112.36
+            }
+            function poundConverter(){
+            document.converter.dollar.value = document.converter.pound.value * 2.049
+            document.converter.euro.value = document.converter.pound.value * 1.394
+            document.converter.yen.value = document.converter.pound.value * 230.27
+            }
+            function yenConverter(){
+            document.converter.dollar.value = document.converter.yen.value * 0.0089
+            document.converter.pound.value = document.converter.yen.value * 0.00434
+            document.converter.euro.value = document.converter.yen.value * 0.00605
+            }
+            
 
 console.log('INFO: Configuring event handlers');
 
